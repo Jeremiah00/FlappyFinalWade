57979941947f1d918cdc5ab6b560cb0905e781df
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2022-12-19 22:52:35 -0800

    Progress
    Added and finished the game control script and started working on the scrolling script.

--- FlappyBirdFinalWade/Assets/Scripts/GameController.cs
+++ FlappyBirdFinalWade/Assets/Scripts/GameController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public static GameController instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
     }
 }
